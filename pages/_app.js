30bb3a5c6f2474e8eccb6ff7e40e2a4dc19daabc
NIAZI77 <hamzaniazi0999@gmail.com>
2024-06-03 04:06:20 -0700

    og

--- pages/_app.js
+++ pages/_app.js
@@ -47,6 +47,12 @@ export default function App({ Component, pageProps }) {
         <meta property="og:image" content="https://tennsy.vercel.app/logo.svg" />
         <meta property="og:url" content="https://tennsy.vercel.app/" />
         <meta property="og:site_name" content="Tennsy" />
+        <meta property="twitter:title" content="Wear Your Attitude - Tennsy" />
+        <meta property="twitter:description" content="Shop exclusive t-shirt designs that speak volumes about your attitude. Explore trending styles, recent launches, and best sellers at Tennsy." />
+        <meta property="twitter:type" content="website" />
+        <meta property="twitter:image" content="https://tennsy.vercel.app/ltwittero.svg" />
+        <meta property="twitter:url" content="https://tennsy.vercel.app/" />
+        <meta property="twitter:site_name" content="Tennsy" />
       </Head>
       <Navbar />
       <div className="md:max-w-[calc(100vw-15px)] w-screen min-h-screen py-16 overflow-x-hidden">
