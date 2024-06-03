e6ae1e2f93c6850ad8acf13805265c570e513185
NIAZI77 <hamzaniazi0999@gmail.com>
2024-06-03 04:16:09 -0700

    og

--- pages/_app.js
+++ pages/_app.js
@@ -34,20 +34,20 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <link rel="shortcut icon" href="/logo.svg" type="image/svg" />
-        <title>Wear Your Atitude - Tennsy</title>
+        <title>Express Your Attitude with Exclusive T-Shirt Designs - Tennsy</title>
         <meta
           name="description"
           content="Shop exclusive t-shirt designs that speak volumes about your attitude. Explore trending styles, recent launches, and best sellers at Tennsy."
         />
         <link rel="canonical" href="https://tennsy.vercel.app/" />
         <meta name="description" content="Shop exclusive t-shirt designs that speak volumes about your attitude. Explore trending styles, recent launches, and best sellers at Tennsy." />
-        <meta property="og:title" content="Wear Your Attitude - Tennsy" />
+        <meta property="og:title" content="Express Your Attitude with Exclusive T-Shirt Designs - Tennsy" />
         <meta property="og:description" content="Shop exclusive t-shirt designs that speak volumes about your attitude. Explore trending styles, recent launches, and best sellers at Tennsy." />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://tennsy.vercel.app/logo.svg" />
         <meta property="og:url" content="https://tennsy.vercel.app/" />
         <meta property="og:site_name" content="Tennsy" />
-        <meta property="twitter:title" content="Wear Your Attitude - Tennsy" />
+        <meta property="twitter:title" content="Express Your Attitude with Exclusive T-Shirt Designs - Tennsy" />
         <meta property="twitter:description" content="Shop exclusive t-shirt designs that speak volumes about your attitude. Explore trending styles, recent launches, and best sellers at Tennsy." />
         <meta property="twitter:type" content="website" />
         <meta property="twitter:image" content="https://tennsy.vercel.app/ltwittero.svg" />
